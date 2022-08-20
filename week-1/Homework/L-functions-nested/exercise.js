b6ba0434fbab074a4f9c7e96105016260c7554cd
Oswaldo Viloria <oswaldovil42@gmail.com>
2022-08-20 13:37:01 +0200

    ejercicio L resuelto

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,1 +1,18 @@
-function metores(){}
+const mentor1 = "Daniel"
+const mentor2 = "Irina"
+const mentor3 = "Mimi"
+const mentor4 = "Rob"
+const mentor5 = "Yohannes"
+const saludoEnMayusculas = "hello my name is"
+
+function nombresEnMayusculas(mentor) {
+    return mentor.toUpperCase()
+}
+function greeting(a){
+    return a.toUpperCase()
+}
+console.log(`${greeting(saludoEnMayusculas)} ${nombresEnMayusculas(mentor1)}`)
+console.log(`${greeting(saludoEnMayusculas)} ${nombresEnMayusculas(mentor2)}`)
+console.log(`${greeting(saludoEnMayusculas)} ${nombresEnMayusculas(mentor3)}`)
+console.log(`${greeting(saludoEnMayusculas)} ${nombresEnMayusculas(mentor4)}`)
+console.log(`${greeting(saludoEnMayusculas)} ${nombresEnMayusculas(mentor5)}`)
