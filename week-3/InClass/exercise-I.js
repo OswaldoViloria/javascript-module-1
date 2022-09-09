afe8ef8fdd37fe4eca9856cb4de4121a5caf2c60
Oswaldo Viloria <oswaldovil42@gmail.com>
2022-09-09 15:49:39 +0200

    Finish de Homework Week - 3

---
+++ week-3/InClass/exercise-I.js
@@ -0,0 +1,6 @@
+const orderValues = [ 100, "iSMael", 55, 45, "sANyiA", 66, "JaMEs", "eLAmIn", 23, "IsMael", 67, 19, "ElaMIN", ];
+
+const exerciseI = orderValues.slice()
+
+console.log(exerciseI);
+
